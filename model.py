b17e943a3411980cd63c71caba0c5b55276a213e
arpan-dasgupta <arpandasgupta0@gmail.com>
2020-07-07 01:28:32 +0530

    Commented model.py

--- model.py
+++ model.py
@@ -1,8 +1,38 @@
-from sklearn import neural_network
+from sklearn import neural_network, linear_model
 import numpy as np
 
 
 class Linear:
+    """
+    Model class 
+    Can be replaced by any other model with same functions
+    """
+
+    def __init__(self, num_models, num_features):
+        self.models = [linear_model.LinearRegression()
+                       for i in range(num_models)]
+        self.num_models = num_models
+        x_train_single = [np.random.random() for i in range(num_features)]
+        for i in range(num_models):
+            self.models[i].fit(x_train_single, np.random.random())
+
+    def get_predictions(self, x_train_single):
+        """
+        Get predictions for a single row of features
+        """
+        h = list()
+        for i in range(self.num_models):
+            h.append(self.models[i].predict(x_train_single))
+
+    def update(self, x_train_single, updated_h):
+        """
+        Train a single step with the updated_h as a list of expected output
+        """
+        for i in range(self.num_models):
+            self.models[i].partial_fit(x_train_single, updated_h[i])
+
+
+class MLP:
 
     def __init__(self, num_models, num_features):
         self.models = [neural_network.MLPRegressor()
