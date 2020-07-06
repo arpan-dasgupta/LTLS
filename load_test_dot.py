1fb7e783ad4bf7919018a47e6ee4344317d8c7a4
arpan-dasgupta <arpandasgupta0@gmail.com>
2020-07-07 02:18:47 +0530

    Updated the test/train loaders

--- load_test_dot.py
+++ load_test_dot.py
@@ -16,56 +16,53 @@ test_specs = {}
 
 
 def get_test_data():
-	# READ TEST FILE
-	f = open(TEST_FILE_PATH)
-	size = f.readline()
-	nrows, nfeature, nlabel = [int(s) for s in size.split()]
-	test_specs['test_length'] = nrows
-	x_m = [[] for i in range(nrows)]
-	pos = [[] for i in range(nrows)]
-	y_m = [[] for i in range(nrows)]
-	for i in range(nrows):
-		line = f.readline()
-		temp=[s for s in line.split(sep=' ')]
-		pos[i]=[int(s.split(':')[0]) for s in temp[1:]]
-		x_m[i]=[float(s.split(':')[1]) for s in temp[1:]]
-		for s in temp[0].split(','):
-			try:
-				int(s)
-				y_m[i]=[ int(s) for s in temp[0].split(',')]
-			except:
-				y_m[i]=[]
+    # READ TEST FILE
+    f = open(TEST_FILE_PATH)
+    size = f.readline()
+    nrows, nfeature, nlabel = [int(s) for s in size.split()]
+    test_specs['test_length'] = nrows
+    x_m = [[] for i in range(nrows)]
+    pos = [[] for i in range(nrows)]
+    y_m = [[] for i in range(nrows)]
+    for i in range(nrows):
+        line = f.readline()
+        temp = [s for s in line.split(sep=' ')]
+        pos[i] = [int(s.split(':')[0]) for s in temp[1:]]
+        x_m[i] = [float(s.split(':')[1]) for s in temp[1:]]
+        for s in temp[0].split(','):
+            try:
+                int(s)
+                y_m[i] = [int(s) for s in temp[0].split(',')]
+            except:
+                y_m[i] = []
 
-	x_test=sparse.lil_matrix((nrows,nfeature))
-	for i in range(nrows):
-		for j in range(len(pos[i])):
-			x_test[i,pos[i][j]]=x_m[i][j]
+    x_test = sparse.lil_matrix((nrows, nfeature))
+    for i in range(nrows):
+        for j in range(len(pos[i])):
+            x_test[i, pos[i][j]] = x_m[i][j]
 
-	del x_m, pos
-	gc.collect()
+    del x_m, pos
+    gc.collect()
 
-	y_test=sparse.lil_matrix((nrows,nlabel))
-	for i in range(nrows):
-		for j in y_m[i]:
-			y_test[i,j]=1
+    y_test = sparse.lil_matrix((nrows, nlabel))
+    for i in range(nrows):
+        for j in y_m[i]:
+            y_test[i, j] = 1
 
-	del y_m
-	gc.collect()
-	f.close()
+    del y_m
+    gc.collect()
+    f.close()
 
-	with open('nozero_col.pkl','rb') as f:
-		nonzeroColindices = pickle.load(f)
-	print("test",len(nonzeroColindices))
-	y_test = y_test[:,nonzeroColindices]
-	x_test = normalize(x_test,norm = 'l2',axis =1,copy=False)
+    x_test = normalize(x_test, norm='l2', axis=1, copy=False)
 
-	test_specs = {
-		'test_length' : x_test.shape[0],
-	}
+    test_specs = {
+        'test_length': x_test.shape[0],
+    }
 
-	print("Testing data is created ")
-	
-	return train_specs, x_test, y_test
+    print("Testing data is created ")
 
-if __name__=="__main__":
+    return test_specs, x_test, y_test
+
+
+if __name__ == "__main__":
     get_test_data()
--- load_train_dot.py
+++ load_train_dot.py
@@ -13,64 +13,59 @@ Header Line: Total_Points Num_Features Num_Labels
 
 train_specs = {}
 
+
 def get_train_data():
-	# READ TRAINING FILE
-	f = open(TRAIN_FILE_PATH)
-	size = f.readline()
-	nrows, nfeature,nlabel = [int(s) for s in size.split()]
-	# train_specs['train_length'] = nrows
-	# train_specs['num_features'] = nfeature
-	# train_specs['num_labels'] = nlabel
-	x_m = [[] for i in range(nrows)]
-	pos = [[] for i in range(nrows)]
-	y_m = [[] for i in range(nrows)]
-	for i in range(nrows):
-		line = f.readline()
-		temp=[s for s in line.split(sep=' ')]
-		pos[i]=[int(s.split(':')[0]) for s in temp[1:]]
-		x_m[i]=[float(s.split(':')[1]) for s in temp[1:]]
-		for s in temp[0].split(','):
-			try:
-				int(s)
-				y_m[i]=[ int(s) for s in temp[0].split(',')]
-			except:
-				y_m[i]=[]
+    # READ TRAINING FILE
+    f = open(TRAIN_FILE_PATH)
+    size = f.readline()
+    nrows, nfeature, nlabel = [int(s) for s in size.split()]
+    # train_specs['train_length'] = nrows
+    # train_specs['num_features'] = nfeature
+    # train_specs['num_labels'] = nlabel
+    x_m = [[] for i in range(nrows)]
+    pos = [[] for i in range(nrows)]
+    y_m = [[] for i in range(nrows)]
+    for i in range(nrows):
+        line = f.readline()
+        temp = [s for s in line.split(sep=' ')]
+        pos[i] = [int(s.split(':')[0]) for s in temp[1:]]
+        x_m[i] = [float(s.split(':')[1]) for s in temp[1:]]
+        for s in temp[0].split(','):
+            try:
+                int(s)
+                y_m[i] = [int(s) for s in temp[0].split(',')]
+            except:
+                y_m[i] = []
 
-	x_train=sparse.lil_matrix((nrows,nfeature))
-	for i in range(nrows):
-		for j in range(len(pos[i])):
-			x_train[i,pos[i][j]]=x_m[i][j]
+    x_train = sparse.lil_matrix((nrows, nfeature))
+    for i in range(nrows):
+        for j in range(len(pos[i])):
+            x_train[i, pos[i][j]] = x_m[i][j]
 
-	del x_m, pos
-	gc.collect()
+    del x_m, pos
+    gc.collect()
 
-	y_train=sparse.lil_matrix((nrows,nlabel))
-	for i in range(nrows):
-		for j in y_m[i]:
-			y_train[i,j]=1
+    y_train = sparse.lil_matrix((nrows, nlabel))
+    for i in range(nrows):
+        for j in y_m[i]:
+            y_train[i, j] = 1
 
-	del y_m
-	gc.collect()
-	f.close()
+    del y_m
+    gc.collect()
+    f.close()
 
-	nonzeroColindices = sorted(list(set(y_train.nonzero()[1])))
-	print(nonzeroColindices[:10])
-	with open('nozero_col.pkl','wb') as p:
-		pickle.dump(nonzeroColindices, p)
-	print("train",len(nonzeroColindices))
-	y_train = y_train[:,nonzeroColindices]
-	x_train = normalize(x_train,norm = 'l2',axis =1,copy=False)
+    x_train = normalize(x_train, norm='l2', axis=1, copy=False)
 
-	train_specs = {
-		'train_length' : x_train.shape[0],
-		'num_features' : x_train.shape[1],
-		'num_labels' : y_train.shape[1]
-	}
+    train_specs = {
+        'train_length': x_train.shape[0],
+        'num_features': x_train.shape[1],
+        'num_labels': y_train.shape[1]
+    }
 
-	print("Training dataset is created")
+    print("Training dataset is created")
 
-	return train_specs, x_train, y_train
+    return train_specs, x_train, y_train
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     get_train_data()
