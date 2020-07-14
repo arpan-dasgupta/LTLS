ccf31ea6c1a2821cc1b54eb9f477cef38b0ef231
Arpan <arpandasgupta0@gmail.com>
2020-07-15 00:16:08 +0530

    Updated paths

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-TRAIN_FILE_PATH = "Bibtex/Bibtex_data.txt"
-TEST_FILE_PATH = "Bibtex/Bibtex_data.txt"
-
 DATA_FILE_PATH = "Bibtex/Bibtex_data.txt"
 TEST_SPLITS = "Bibtex/bibtex_tstSplit.txt"
 TRAIN_SPLITS = "Bibtex/bibtex_trSplit.txt"
 EPOCHS = 5
+
+TEST_FILE_PATH = "Eurlex/eurlex_test.txt"
+TRAIN_FILE_PATH = "Eurlex/eurlex_train.txt"
+#T_FILE_PATH = "WikiLSHTC/wikiLSHTC_test.txt"
+#TRAIN_FILE_PATH = "WikiLSHTC/wikiLSHTC_train.txt"
 
 LOSS_THRESHOLD = 1
 ADD_VALUE = 1
